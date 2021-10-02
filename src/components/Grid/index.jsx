076b0181ac911d70880bc4f1e2025b77091f2f87
kailash360 <kailashkejriwal21@gmail.com>
2021-10-02 14:02:25 +0530

    replaced components with map() function

--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -5,37 +5,24 @@ import {ReactComponent as Y} from "../../assets/2x2.svg";
 import {ReactComponent as Z} from "../../assets/3x3.svg";
 
 const Grid = ({ view, setView }) => {
+
+  // Required to render the svgs  
+  const list = [<Z/>,<Y/>,<X/>]
+
   return (
     <div className={styles.gridAlign}>
-      {/* {[z, y, x].map((img, i) => ( */}
+      {[Z, Y, X].map((image, i) => (
         <a
-          key={0}
-          onClick={() => setView(0 + 1)}
-          className={0 + 1 === view ? styles.selected : null}
+          key={i}
+          onClick={() => setView(i + 1)}
+          className={i + 1 === view ? styles.selected : null}
           href="#!"
         >
-          {/* <img src={img} alt={`${3 - i}x${3 - i}`} /> */}
-          <Z/>
+          {/* img tag cannot be used here since the colour of svg
+          needs to change according to the theme  */}
+          {list[i]}
         </a>
-        <a
-          key={1}
-          onClick={() => setView(1 + 1)}
-          className={1 + 1 === view ? styles.selected : null}
-          href="#!"
-        >
-          {/* <img src={img} alt={`${3 - i}x${3 - i}`} /> */}
-          <Y/>
-        </a>
-        <a
-          key={2}
-          onClick={() => setView(2 + 1)}
-          className={2 + 1 === view ? styles.selected : null}
-          href="#!"
-        >
-          {/* <img src={img} alt={`${3 - i}x${3 - i}`} /> */}
-          <X/>
-        </a>
-      {/* ))} */}
+       ))} 
     </div>
   );
 };
