91169a628f6c77ebfd6870d1ff3f366a31513f69
Nishidha Sri <44313138+MeekMo@users.noreply.github.com>
2021-10-28 17:47:13 +0530

    new branch to check development

--- src/App.js
+++ src/App.js
@@ -3,24 +3,23 @@ import "./App.css";
 import Navigation from "./components/Navigation/index";
 import Favorites from "./pages/Favorites/index";
 import Home from "./pages/Home/index";
-import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme } from './ThemeChanger/themes';
-import { GlobalStyles } from './ThemeChanger/global';
-import { useDarkMode } from './ThemeChanger/useDarkMode'
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./ThemeChanger/themes";
+import { GlobalStyles } from "./ThemeChanger/global";
+import { useDarkMode } from "./ThemeChanger/useDarkMode";
 
 function App() {
-
-  const [theme,toggleTheme] = useDarkMode()
+  const [theme, toggleTheme] = useDarkMode();
   return (
     <>
-    <ThemeProvider theme={theme==='dark'?lightTheme:darkTheme}>
-      <GlobalStyles/>
-      <Navigation theme={theme} toggleTheme={toggleTheme} />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/liked" component={Favorites} />
-      </Switch>
-    </ThemeProvider>
+      <ThemeProvider theme={theme === "dark" ? lightTheme : darkTheme}>
+        <GlobalStyles />
+        <Navigation theme={theme} toggleTheme={toggleTheme} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/liked" component={Favorites} />
+        </Switch>
+      </ThemeProvider>
     </>
   );
 }
